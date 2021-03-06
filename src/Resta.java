a6c215440452cb9c79053abd0558bdccbb379005
Jesús Rodríguez Herranz <jesusrodriguezherranz@gmail.com>
2021-03-07 00:50:39 +0100

    Actualizo la Clase Resta, arreglando fallos en los métodos y añadiendo la documentación pertinente

--- src/Resta.java
+++ src/Resta.java
@@ -1,5 +1,5 @@
 /**
- * Esta clase contendr� los m�todos que realizar�n las restas en la c�lculadora
+ * Esta clase contendra los metodos que realizaran las restas en la calculadora
  * 
  * @author Jesus Rodriguez Herranz
  */
@@ -7,17 +7,45 @@ public class Resta {
 	
 	private static int acumulador = 0;
 
+	/**
+	 * Este metodo sirve para restar dos numeros enteros
+	 * 
+	 * @param valor1 Contendra el primer numero entero de la resta
+	 * @param valor2 Contendra el primer numero entero de la resta
+	 * @return devolvera el restultado de la resta
+	 */
+	
 	public static int resta(int valor1, int valor2) {
-		return valor1 + valor2;
+		return valor1 - valor2;
 	}
 	
-	public static double resta(double valor1, double valor2, double valor3) {
-		return valor1 + valor2 + valor2;
+	/**
+	 * Este metodo sirve para restar tres numeros reales
+	 * 
+	 * @param valor1 Contendra el primer numero real de la resta
+	 * @param valor2 Contendra el segundo numero real de la resta
+	 * @param valor3 Contendra el tercer numero real de la resta
+	 * @return devolvera el restultado de la resta
+	 */
+	
+	public static double resta(double valor1, double valor2,) {
+		return valor1 - valor2;
 	}
+	
+	/**
+	 * Este metodo sirve para restar tres numeros enteros
+	 * 
+	 * @param valor1 Contendra el primer numero entero de la resta
+	 * @param valor2 Contendra el segundo numero entero de la resta
+	 * @param valor3 Contendra el tercer numero entero de la resta
+	 * @return devolvera el restultado de la resta
+	 */
 	
 	public static int resta(int valor1, int valor2, int valor3) {
 		return valor1 + valor2 + valor2;
 	}
+	
+	
 	
 	public static int restaAcumulado(int nuevovalor) {
 		acumulador -= nuevovalor;
