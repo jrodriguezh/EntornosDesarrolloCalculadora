2006dd594b2edba6224778701f13c5f1f0b724c6
Jesús Rodríguez Herranz <jesusrodriguezherranz@gmail.com>
2021-03-07 00:32:59 +0100

    Actualizo la clase Resta, añado los métodos que utilizaremos
    Implemento los métodos que utilizaremos para realizar los cálculos referentes a las restas en la calculadora, queda pendiente realizar la documentación. Y comprobar su integridad.

--- src/Resta.java
+++ src/Resta.java
@@ -1,10 +1,27 @@
 /**
+ * Esta clase contendrï¿½ los mï¿½todos que realizarï¿½n las restas en la cï¿½lculadora
  * 
- */
-
-/**
- * @author Jesús Rodríguez Herranz
+ * @author Jesus Rodriguez Herranz
  */
 public class Resta {
+	
+	private static int acumulador = 0;
 
+	public static int resta(int valor1, int valor2) {
+		return valor1 + valor2;
+	}
+	
+	public static double resta(double valor1, double valor2, double valor3) {
+		return valor1 + valor2 + valor2;
+	}
+	
+	public static int resta(int valor1, int valor2, int valor3) {
+		return valor1 + valor2 + valor2;
+	}
+	
+	public static int restaAcumulado(int nuevovalor) {
+		acumulador -= nuevovalor;
+		return acumulador;
+
+	}
 }
