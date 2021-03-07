865b77be557d3aa1c0dba80f01961041b5698acc
Jesús Rodríguez Herranz <jesusrodriguezherranz@gmail.com>
2021-03-07 01:27:44 +0100

    Creo la clase Cociente, la cual contendrá los métodos necesarios para realizar los cálculos de las divisiones. Al mismo tiempo, desarrollo los métodos para la realización de una división de dos números enteros, dos números reales, así como para calcular el inverso. Comienzo con la edición de la documentación específica de los métodos

---
+++ src/Cociente.java
@@ -0,0 +1,56 @@
+/**
+ * Esta clase contendra los metodos que realizaran las divisiones en la
+ * calculadora
+ * 
+ * @author Jesus Rodriguez Herranz
+ */
+
+public class Cociente {
+	
+	/**
+	 * Este metodo sirve para realizar la division de dos numeros reales
+	 * 
+	 * @param valor1 Contendra el primer numero real de la division
+	 * @param valor2 Contendra el segundo numero real de la division
+	 * @return devolvera el resultado de la division
+	 */
+
+	public static double division(double valor1, double valor2) {
+		return valor1 / valor2;
+	}
+
+	/**
+	 * Este metodo sirve para realizar la division de dos numeros enteros<br>
+	 * 
+	 * Casos especiales:<br>
+	 * 
+	 * No es posible dividir entre cero, puesto que daraa lugar a un numero
+	 * infinito, Si divides un numero entre 0 dara lugar a un error tipo
+	 * java.lang.ArithmeticException: / by zero
+	 * 
+	 * 
+	 * 
+	 * <br>
+	 * 
+	 * @param valor1 Contendra el primer numero entero de la division
+	 * @param valor2 Contendra el segundo numero entero de la division
+	 * @return devolvera el resultado de la division
+	 */
+
+	public static int division(int valor1, int valor2) {
+		return valor1 / valor2;
+	}
+
+	/**
+	 * Este metodo sirve para calcular el inverso "1/x siendo x el numero" de un
+	 * numero
+	 * 
+	 * @param valor1 Contendra el valor del numero a invertir
+	 * @return devolvera el resultado de la inversion
+	 */
+
+	public static int inverso(int valor1) {
+		return 1 / valor1;
+	}
+
+}
