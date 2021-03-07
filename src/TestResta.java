f5efa5cf50ad11830766c65ce0fbf81fd68b7e36
Alejandro <71126537+Folen8@users.noreply.github.com>
2021-03-07 19:54:07 +0100

    Realizados los test de la clase Resta favorablemente
    Habría que modificar la descripción en el JavaDoc en el segundo método.

--- src/TestResta.java
+++ src/TestResta.java
@@ -36,7 +36,7 @@ class TestResta {
 	void testRestaDoubleDoubleDosNumerosNegativos() {
 		// 1 Arrange - Preparar
 		// 2 Act - Ejecutar
-		double esperado= 1.50;
+		double esperado = 1.50;
 		double resultado = Resta.resta(-1.5, -3.00);
 		// 3 Assert - Comprobar
 		assertEquals(esperado, resultado);
@@ -46,18 +46,31 @@ class TestResta {
 	 * Este test comprobara si existen errores a la hora de realizar una resta entre
 	 * dos números reales {@link Resta#resta(double, double, double)}
 	 */
-	
+
 	@Test
 	void testRestaDoubleDoubleDouble() {
-		fail("Not yet implemented");
+		// 1 Arrange - Preparar
+		// 2 Act - Ejecutar
+		double esperado = 2.50;
+		double resultado = Resta.resta(-1.0, -1.0, -2.5);
+		// 3 Assert - Comprobar
+		assertEquals(esperado, resultado);
 
-		
 	}
+
+	/**
+	 * Este test comprobara si existen errores a la hora de realizar una resta con acumulado {@link Resta#restaAcumulado(int)}
+	 */
 
 	@Test
 	void testRestaAcumulado() {
-		fail("Not yet implemented");
+		// 1 Arrange - Preparar;
+		// 2 Act - Ejecutar
+		int esperado = -15;
+		int prueba1 = Resta.restaAcumulado(30);
+		int resultado = Resta.restaAcumulado(-15);
+		// 3 Assert - Comprobar
+		assertEquals(esperado, resultado);
 	}
 
 }
-
